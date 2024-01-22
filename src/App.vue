eb426d1078d3d9d50d49cae9726da8e9119afa79
Lwin Maung Maung <lwindev97xlance@gmail.com>
2024-01-22 16:15:27 +0630

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+    <ul>
+        <learning-resource v-for="resource in storedResources" :key="resource.id" :resource-title="resource.title"
+            :resource-description="resource.description" :resource-link="resource.link">
+        </learning-resource>
+    </ul>
+</template>
+
+<script>
+import LearningResource from "./components/LearningResource.vue";
+
+export default {
+    components: {
+        LearningResource,
+    },
+    data() {
+        return {
+            storedResources: [
+                {
+                    id: "1234",
+                    title: "Official Guide",
+                    description: "The official Vue.js documentation.",
+                    link: "https://vuejs.org",
+                },
+                {
+                    id: "google",
+                    title: "Google",
+                    description: "Learn to google...",
+                    link: "https://google.org",
+                },
+            ],
+        };
+    }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+    box-sizing: border-box;
+}
+
+html {
+    font-family: 'Roboto', sans-serif;
+}
+
+body {
+    margin: 0;
+}
+</style>
---
+++ src/components/LearningResource.vue
@@ -0,0 +1,30 @@
+<template>
+    <li>
+        <div>
+            <header>
+                <h3>{{ resourceTitle }}</h3>
+                <button>Delete</button>
+            </header>
+        </div>
+        <p>{{ resourceDescription }}</p>
+        <nav>
+            <a :href="resourceLink">View Resource</a>
+        </nav>
+    </li>
+</template>
+
+<script>
+export default {
+    props: {
+        resourceTitle: String,
+        resourceDescription: String,
+        resourceLink: String,
+    }
+}
+</script>
+
+<style scoped>
+section {
+    background-color: red;
+}
+</style>
